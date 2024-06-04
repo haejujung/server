d245e138208e52b74d1efd8187459cc46b2dcc44
haeju0524 <ysin70@naver.com>
2024-06-04 14:35:13 +0900

    server_class

---
+++ src/ch05/MyThreadServer.java
@@ -0,0 +1,29 @@
+package ch05;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class MyThreadServer extends AbstractServer {
+
+	@Override
+	protected void setupServer() throws IOException {
+		// 추상 클래스 --> 부모 -- 자식 (부모 기능에 확장 또는 사용)
+		// 서버측 소켓 통신 -- 준비물 : 서버 소켓
+		super.setServerSocket(new ServerSocket(5000));
+		System.out.println(">>> 5000 포트에서 서버 시작 <<<");
+	}
+
+	@Override
+	protected void connection() throws IOException {
+		// 서버 소켓.accept() 호출이다. !!!
+		super.setSocket(super.getServerSocket().accept());
+	}
+
+	public static void main(String[] args) {
+
+		MyThreadServer myThreadServer = new MyThreadServer();
+		myThreadServer.run();
+
+	}
+
+}
